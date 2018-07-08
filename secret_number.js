ef59582e6305aa6190ae377b2f907a38c1d63827
JEFrady <jamie.e.halliday@gmail.com>
2018-07-07 15:23:27 -1000

    All tests passing

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
 module.exports = function() {
-  
+    let number = Math.floor(Math.random() * Math.floor(1000000));
+    return function secretNumberGenerator() {
+        return number;
+    }
 };
+
+
